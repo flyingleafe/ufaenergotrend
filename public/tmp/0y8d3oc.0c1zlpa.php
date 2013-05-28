7fe37f76e76470e7ef5bc7f9bacffbf183767e93
FlyingLeafe <flyingleafe@gmail.com>
2013-05-28 12:23:57 +0600

    credits

--- public/tmp/0y8d3oc.0c1zlpa.php
+++ public/tmp/0y8d3oc.0c1zlpa.php
@@ -1,12 +1,14 @@
-<footer>
-    <div class="container">
+c<footer>
+    <div class="container clearfix">
         <ul class="column">
             <li><a href="/blog">Блог компании</a></li>
             <li><a href="/about">О нас</a></li>
             <li><a href="/contacts">Контакты</a></li>
         </ul>
         <div class="column"></div>
-        <div class="credits"></div>
-        <div class="clearfix"></div>
+        <div class="column"></div>
+        <div class="credits">
+            created by <a href="http://freelansim.ru/freelancers/flyingleafe">flyingleafe</a>
+        </div>
     </div>
 </footer>
